d8d172289f6903b403af21fe0b3691a2dfb539e2
Chris <scientificharmony@gmail.com>
2026-05-18 12:59:32 +0100

    fix: correct firebase_messaging import package name

--- lproject/lib/services/fcm_service.dart
+++ lproject/lib/services/fcm_service.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:firebasemessaging/firebasemessaging.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
 import '../config/api_config.dart';
